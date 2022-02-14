2a41834ffdc03437cec40818afc9764778720b2f
RedCrafter07 <github@redcrafter07.de>
2022-02-14 13:35:43 +0100

    Formatting, Scheduled Events & Ping Roles

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const Discord = require('discord.js');
 const { Intents, Collection, Permissions } = Discord;
 const client = new Discord.Client({
-	intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_SCHEDULED_EVENTS, Intents.FLAGS.GUILD_MESSAGES ]
+	intents : [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
+		Intents.FLAGS.GUILD_MESSAGES,
+	],
 });
 const chalk = require('chalk');
+const moment = require('moment');
 const EZTwitch = require('@redcrafter07/eztwitch');
 const config = require('./config.json');
-const twitchClient = new EZTwitch.client().setID(config.id).setSecret(config.secret).setToken(config.twitchToken);
+const twitchClient = new EZTwitch.client()
+	.setID(config.id)
+	.setSecret(config.secret)
+	.setToken(config.twitchToken);
 
 const STREAM_CONFIG = require('./stream.config.js');
 
 let guild;
 let channel;
 let notifyMsg;
+let event;
+let live = false;
 
 client.login(config.discordToken);
 
@@ -29,28 +39,36 @@ client.on('ready', async () => {
 
 async function checkForMessage() {
 	guild = await client.guilds.cache.get(STREAM_CONFIG.discord.mainGuild);
-	channel = await guild.channels.cache.get(STREAM_CONFIG.discord.notificationChannel);
+
+	channel = await guild.channels.cache.get(
+		STREAM_CONFIG.discord.notificationChannel
+	);
 	if (!channel.isText()) return;
 	await channel.messages.fetch();
-	notifyMsg = await channel.messages.cache.find(m => m.author.id == client.user.id);
+	notifyMsg = await channel.messages.cache.find(
+		m => m.author.id == client.user.id
+	);
 	console.log(notifyMsg);
 	if (!notifyMsg) {
 		await channel.send({
-			embeds: [
+			embeds : [
 				new Discord.MessageEmbed()
 					.setTitle('Stream Notifications')
 					.setDescription(
 						'This message is not setuped. The setup process will take about 20 Seconds. It should update itself automatically. :)'
 					)
-					.setColor(STREAM_CONFIG.colors.offline)
-			]
+					.setColor(STREAM_CONFIG.colors.offline),
+			],
 		});
 		notifyMsg = channel.messages.cache.find(m => m.author.id == client.user.id);
 	}
 }
 
 async function updateDiscord() {
-	let streamData = await fetchStream(STREAM_CONFIG.twitch.channel, twitchClient);
+	let streamData = await fetchStream(
+		STREAM_CONFIG.twitch.channel,
+		twitchClient
+	);
 	let embed = await embedBuilder(streamData);
 	// console.log(embed);
 	if (notifyMsg) await notifyMsg.edit({ embeds: [ embed ] });
@@ -62,17 +80,50 @@ async function embedBuilder(streamData) {
 	let embed = new Discord.MessageEmbed();
 
 	if (online) {
-		embed.setTitle(`${streamData.title}`);
+		if (!event && STREAM_CONFIG.notifications.useScheduledEvents == true)
+			event = await guild.scheduledEvents.create({
+				name               : `${STREAM_CONFIG.twitch.channel} is live!`,
+				reason             : 'Stream is live.',
+				description        : `${STREAM_CONFIG.twitch.channel} is live!`,
+				entityType         : 'EXTERNAL',
+				scheduledStartTime : moment().add(1, 's').toDate(),
+				scheduledEndTime   : moment()
+					.add(STREAM_CONFIG.twitch.checkInterval, 'ms')
+					.toDate(),
+				privacyLevel       : 'GUILD_ONLY',
+				entityMetadata     : {
+					location : `https://twitch.tv/${STREAM_CONFIG.twitch.channel}`,
+				},
+			});
+
+		try {
+			if (event)
+				await event.edit({
+					scheduledEndTime : moment()
+						.add(STREAM_CONFIG.twitch.checkInterval, 'ms')
+						.toDate(),
+				});
+		} catch (err) {}
+
+		embed.setTitle(`${streamData.title ? streamData.title : 'Untitled'}`);
 		embed.setURL(`https://twitch.tv/${streamData.login}`);
-		embed.setThumbnail(`${streamData.thumbnail}`);
-		embed.setImage(`${EZTwitch.utils.thumbnailSize(streamData.thumbnail, 1920, 1080)}?x=${new Date().getTime()}`);
+		if (streamData.thumbnail && streamData.thumbnail != '')
+			embed.setThumbnail(`${streamData.thumbnail}`);
+		if (streamData.thumbnail && streamData.thumbnail != '')
+			embed.setImage(
+				`${EZTwitch.utils.thumbnailSize(
+					streamData.thumbnail,
+					1920,
+					1080
+				)}?x=${new Date().getTime()}`
+			);
 		embed.addFields([
 			{ name: 'Viewers', value: `${streamData.viewers}`, inline: true },
 			{
-				name: 'Game',
-				value: `${streamData.game}`,
-				inline: true
-			}
+				name   : 'Game',
+				value  : `${streamData.game ? streamData.game : '-'}`,
+				inline : true,
+			},
 		]);
 
 		embed.setColor;
@@ -82,25 +133,40 @@ async function embedBuilder(streamData) {
 			STREAM_CONFIG.twitch.timestampFormat
 		)}`;
 		embed.setColor(STREAM_CONFIG.colors.online);
+		if (live === false && STREAM_CONFIG.notifications.pingRole == true) {
+			let msg = await channel.send(
+				`<@&${STREAM_CONFIG.discord.notificationRole}>`
+			);
+			msg.delete();
+		}
+		live = true;
 	} else {
-		embed.setTitle('Offline').setColor(STREAM_CONFIG.colors.offline).setImage(streamData.offlineImage).addFields([
-			{
-				name: 'Views',
-				value: `${streamData.views}`,
-				inline: false
-			},
-			{
-				name: 'Description',
-				value: `${streamData.description}`
-			}
-		]);
+		try {
+			if (event) await event.setStatus('COMPLETED');
+		} catch (err) {}
+		if (event) event = undefined;
+		embed
+			.setTitle('Offline')
+			.setColor(STREAM_CONFIG.colors.offline)
+			.setImage(streamData.offlineImage)
+			.addFields([
+				{
+					name   : 'Views',
+					value  : `${streamData.views}`,
+					inline : false,
+				},
+				{
+					name  : 'Description',
+					value : `${streamData.description ? streamData.description : '-'}`,
+				},
+			]);
 		embed.footer = `Created at ${EZTwitch.utils.formatTimestamp(
 			streamData.createdAt,
 			STREAM_CONFIG.twitch.timestampFormat
 		)}`;
 		embed.author = {
-			name: `${streamData.display}`,
-			iconURL: `${streamData.profileImage}`
+			name    : `${streamData.display}`,
+			iconURL : `${streamData.profileImage}`,
 		};
 	}
 
